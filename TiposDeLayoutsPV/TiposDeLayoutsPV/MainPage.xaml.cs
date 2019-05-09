db3419a15c53d3731f3ef791d619c47546580980
Tomás Serrano Mata <TheMoonComeOut@gmail.com>
2019-05-09 19:12:14 +0200

    Tipos de Layouts

--- TiposDeLayoutsPV/TiposDeLayoutsPV/MainPage.xaml.cs
+++ TiposDeLayoutsPV/TiposDeLayoutsPV/MainPage.xaml.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TiposDeLayoutsPV.XamlViews;
 using Xamarin.Forms;
 
-namespace TiposDeLayoutsPV
-{
-    public partial class MainPage : ContentPage
-    {
-        public MainPage()
-        {
+namespace TiposDeLayoutsPV {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
             InitializeComponent();
+            this.botonstack.Clicked += async (sender, args) => await Navigation.PushModalAsync(new StackLayoutView());
+            this.botonscroll.Clicked += async (sender, args) => await Navigation.PushModalAsync(new ScrollViewPage());
+            this.botonframe.Clicked += async (sender, args) => await Navigation.PushModalAsync(new FrameView());
+            this.botonflex.Clicked += async (sender, args) => await Navigation.PushModalAsync(new FlexLayoutView());
+            this.botonabsolute.Clicked += async (sender, args) => await Navigation.PushModalAsync(new AbsoluteLayoutView());
         }
     }
 }
